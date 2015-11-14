f186c0995b3e044ce7b8bb5c4b5a10b114aef399
Michael <borinskym@gmail.com>
2015-11-14 20:00:04 +0200

    adding pizza image

--- app/src/main/java/com/example/alienware/feedmepizza/MainActivity.java
+++ app/src/main/java/com/example/alienware/feedmepizza/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.alienware.feedmepizza;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configureImageButton();
+    }
+
+    private void configureImageButton() {
+        ImageButton imgButton = (ImageButton) findViewById(R.id.imageButton);
+        imgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),Im_picky.class);
+                startActivityForResult(intent,0);
+            }
+        });
     }
 
     @Override
