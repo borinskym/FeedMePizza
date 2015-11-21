d24a6890dd3d11ba203ce4d30a4290e8ebfa7ec3
Michael <borinskym@gmail.com>
2015-11-21 18:25:28 +0200

    support settings view

--- app/src/main/java/com/example/alienware/feedmepizza/screens/MainActivity.java
+++ app/src/main/java/com/example/alienware/feedmepizza/screens/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.alienware.feedmepizza.screens;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,58 +11,85 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
 import com.example.alienware.feedmepizza.R;
 import com.example.alienware.feedmepizza.screens.Im_picky;
 
 public class MainActivity extends AppCompatActivity {
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private DrawerLayout drawerLayout;
+    private ListView navList;
+    private ActionBar actionBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        configureImageButton();
-        configureIAmPickyButton();
-        configureSettingsButton();
+        initializeSettingsBar();
+        actionBar = getSupportActionBar();
+        actionBar.setDefaultDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+      //  configureImageButton();
+       // configureIAmPickyButton();
+       // configureSettingsButton();
     }
 
-    private void configureSettingsButton() {
-        ImageButton imgButton = (ImageButton)findViewById(R.id.settingsButton);
-        imgButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(),Im_picky.class);
-                startActivity(intent);
-                overridePendingTransition(R.anim.right_slide_in,R.anim.right_slide_out);
+    private void initializeSettingsBar() {
+
+         drawerLayout = (DrawerLayout)findViewById(R.id.drawerlayout);
+        navList = (ListView)findViewById(R.id.navlist);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.opendrawer,R.string.closedrawer);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
 
 
-            }
-        });
     }
 
-    private void configureIAmPickyButton() {
-        Button iAmPickyButton = (Button)findViewById(R.id.i_am_picky_button);
-        iAmPickyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(),Im_picky.class);
-                startActivityForResult(intent,0);
-
-            }
-        });
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState){
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+        
     }
 
-    private void configureImageButton() {
-        ImageButton imgButton = (ImageButton) findViewById(R.id.imageButton);
-        imgButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(),Im_picky.class);
-                startActivityForResult(intent,0);
-            }
-        });
-    }
+//    private void configureSettingsButton() {
+//        ImageButton imgButton = (ImageButton)findViewById(R.id.settingsButton);
+//        imgButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(v.getContext(),Im_picky.class);
+//                startActivity(intent);
+//                overridePendingTransition(R.anim.right_slide_in,R.anim.right_slide_out);
+//
+//
+//            }
+//        });
+//    }
+//
+//    private void configureIAmPickyButton() {
+//        Button iAmPickyButton = (Button)findViewById(R.id.i_am_picky_button);
+//        iAmPickyButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(v.getContext(),Im_picky.class);
+//                startActivityForResult(intent,0);
+//
+//            }
+//        });
+//    }
+//
+//    private void configureImageButton() {
+//        ImageButton imgButton = (ImageButton) findViewById(R.id.imageButton);
+//        imgButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(v.getContext(),Im_picky.class);
+//                startActivityForResult(intent,0);
+//            }
+//        });
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -78,6 +108,13 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }else if(id == android.R.id.home) {
+                if(drawerLayout.isDrawerOpen(navList)){
+                    drawerLayout.closeDrawer(navList);
+                }
+                else{
+                    drawerLayout.openDrawer(navList);
+                }
         }
 
         return super.onOptionsItemSelected(item);
